df1ba1d05810f8d8a92a30df66ad021c53cc469f
lindberg <ad.lindberg@gmail.com>
2024-10-15 21:18:26 +0200

    allowed hosts

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -37,8 +37,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost',
-                 '127.0.0.1']
+ALLOWED_HOSTS = [
+    'musicshowcase-90ce543f3669.herokuapp.com',
+    'localhost',  
+    '127.0.0.1',  
+]
+
 
 
 # Application definition
